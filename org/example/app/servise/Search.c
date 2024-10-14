fb19cc1784d16a61611fd7cc882e9174d42e5570
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-10-14 19:12:18 +0300

    modified

--- org/example/app/servise/Search.c
+++ org/example/app/servise/Search.c
@@ -1,20 +1,5 @@
 #include "Search.h"
 
-int search(int *arr, int size, int value) {
-    int left = 0, right = size - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == value) {
-            return mid;
-        } else if (arr[mid] < value) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
-
 int searchMin(int *arr, int len) {
     int min = arr[0];
     for (int i = 1; i < len; i++) {
