7d32004dd7d77506c9e9a362cfde28b7f73e1017
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-10-14 19:06:09 +0300

    relise v1.0.0

--- org/example/app/controll/AppControll.c
+++ org/example/app/controll/AppControll.c
@@ -6,14 +6,23 @@
 
 int run() {
     srand(time(NULL));
-    int min = 1, max = 100;
-    int len = randNum(min, max);
-    int *arr = createArr(len, min, max);
+    int min = 1, max_len = 10;
+    int max_range = 100;
+    int len = randNum(min, max_len);
+
+    int *arr = createArr(len, min, max_range);
     printArr(arr, len);
+
     int *sorted_arr_az = sortArrAZ(arr, len);
     int *sorted_arr_za = sortArrZA(arr, len);
+
     int len_d_arr = len * 2;
     int *doubled_arr = doubleArr(sorted_arr_az, sorted_arr_za, len_d_arr);
     printArr(doubled_arr, len_d_arr);
+
+    free(arr);
+    free(sorted_arr_az);
+    free(sorted_arr_za);
+    free(doubled_arr);
     return 0;
 }
--- org/example/app/utils/Output.c
+++ org/example/app/utils/Output.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <signal.h>
 
 void printArr(int arr[], int len) {
     for (int i = 0; i < len; i++) {
